f5ed7905f2bdf680773fc1d668259cb223b37092
1 <a1234@MacBook-Air-1.local>
2024-08-06 00:30:40 +0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,29 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SoftwareController } from './software/software.controller';
 import { WalletService } from './wallet/wallet.service';
-import { MongooseModule } from '@nestjs/mongoose';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
+
+
+
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URL || 'mongodb://localhost:27017/nestdb'),
-    
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGO_URL || 'mongodb://localhost:27017/nestdb',
+    ),
+
+    ElasticsearchModule.register({
+      node: 'http://localhost:9200', // Elasticsearch server URL
+    }), 
   ],
   controllers: [AppController, SoftwareController],
   providers: [AppService, WalletService],
-
+  exports: [ElasticsearchModule],
 })
 
 export class AppModule {}
+export class ElasticsearchConfigModule {}
+
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
+
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World "do-pave"!';
+  
   }
 }
+
